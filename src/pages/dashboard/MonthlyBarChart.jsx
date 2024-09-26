08718d7e95f3b22a3f2622982532dd73a824ae8a
Ben Zhao <ben.zhao@benzhao.dev>
2024-09-26 16:28:04 -0400

    Default dashboard (#9)
    * working barchart
    
    * main dashboard completed

--- src/pages/dashboard/MonthlyBarChart.jsx
+++ src/pages/dashboard/MonthlyBarChart.jsx
@@ -1,45 +1,59 @@
-import {useEffect, useMemo, useState} from 'react';
+import {useMemo, useState} from 'react';
 
 // material-ui
-import { useTheme } from '@mui/material/styles';
+import {useTheme} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 
 // third-party
 import ReactApexChart from 'react-apexcharts';
-import {useQuery} from "@apollo/client";
-import {AGGREGATE_EXPENDITURES} from "../../api/queries";
-import {dateString, endOfLastMonth, startOfLastMonth} from "../../utils/dates";
+import {endOfLastMonth, startOfLastMonth, startOfMonth} from "../../utils/dates";
+import {useExpenditureAggregate} from "../../api/graph";
 
 // chart options
+const numCategories = 10
 const barChartOptions = {
   chart: {
     type: 'bar',
-    // height: 800,
     toolbar: {
-      show: false
+      show: true
     }
   },
   plotOptions: {
     bar: {
       // columnWidth: '45%',
-      borderRadius: 4,
-      horizontal: true
+      borderRadius: 1,
+      horizontal: true,
+      dataLabels: {
+        position: 'bottom'
+      },
+      barHeight: 22,
     }
   },
-  dataLabels: {
-    enabled: true
-  },
   xaxis: {
-    categories: ['No data'],
     axisBorder: {
-      show: false
+      show: true
     },
     axisTicks: {
-      show: false
+      show: true
     },
   },
   yaxis: {
-    show: true
+    show: true,
+    labels: {
+      style: {
+        fontSize: 14,
+        fontWeight: 400
+      }
+    }
+  },
+  dataLabels: {
+    enabled: true,
+    textAnchor: 'start',
+    offsetX: 0,
+    formatter: function(val, opt) {
+      // return "$" + val.toFixed(2)
+      return opt.config.series[opt.seriesIndex].name + ": $" + val.toFixed(0)// + " / $" + budget[opt.dataPointIndex].toFixed(0)
+    }
   },
   grid: {
     show: false
@@ -48,56 +62,103 @@ const barChartOptions = {
 
 // ==============================|| MONTHLY BAR CHART ||============================== //
 
-export default function MonthlyBarChart() {
+export default function MonthlyBarChart({ slot }) {
   const theme = useTheme();
 
-  const { primary, secondary } = theme.palette.text;
-  const info = theme.palette.info.light;
-
-  const { data } = useQuery(AGGREGATE_EXPENDITURES, {
-    variables: {
-      since: dateString(startOfLastMonth()),
-      until: dateString(endOfLastMonth()),
-      span: "MONTH",
-      groupBy: "BUDGET_CATEGORY"
-    }
+  const { data } = useExpenditureAggregate({
+    since: slot === "this" ? startOfMonth() : startOfLastMonth(),
+    until: slot === "this" ? new Date() : endOfLastMonth(),
+    span: "MONTH",
+    groupBy: "BUDGET_CATEGORY",
   })
 
-  const [series, setSeries] = useState([80, 95, 70, 42, 65, 55, 78]);
-
+  const [spent, setSpent] = useState([]);
+  const [budgeted, setBudgeted] = useState([]);
+  const [categories, setCategories] = useState(['No data']);
   const [options, setOptions] = useState(barChartOptions);
+
+  useMemo(() => {
+    if (data) {
+      const sortedData = data.aggregatedExpenditures
+          .toSorted((a, b) => a.amount - b.amount)
+          .toReversed()
+
+      setCategories(sortedData.slice(0, numCategories)
+          .map(e => e.groupByCategory))
+
+      setSpent(sortedData.slice(0, numCategories)
+          .map(e => e.amount))
+
+      const budget = [7000, 6000, 5500, 5000, 5000, 4000, 3000, 2000, 1500, 1000]
+
+      setBudgeted(budget)
+    }
+  }, [data])
 
   useMemo(() => {
     setOptions((prevState) => ({
       ...prevState,
-      colors: [info],
       xaxis: {
         ...prevState.xaxis,
         labels: {
           style: {
-            colors: secondary
+            colors: theme.palette.text.secondary
           }
+        },
+        categories: categories
+      },
+      yaxis: {
+        ...prevState.yaxis,
+        labels: {
+          ...prevState.yaxis.labels,
+          style: {
+            ...prevState.yaxis.labels.style,
+            colors: theme.palette.text.primary
+          }
+        }
+      },
+      legend: {
+        show: true,
+        showForSingleSeries: true,
+        customLegendItems: ['spent', 'budgeted'],
+        markers: {
+          fillColors: [theme.palette.primary.main, theme.palette.primary.dark]
         }
       }
     }));
-  }, [primary, info, secondary]);
-
-  useMemo(() => {
-    if (data) {
-      let sortedData = data.aggregatedExpenditures.toSorted((a, b) => a.amount - b.amount).toReversed()
-      setSeries(sortedData.slice(0, 10).map(e => e.amount))
-      setOptions((prevState) => ({
-        ...prevState,
-        xaxis: {
-          categories: sortedData.slice(0, 10).map(e => e.groupByCategory)
-        }
-      }))
-    }
-  }, [data])
+  }, [theme, categories]);
 
   return (
     <Box id="chart" sx={{ bgcolor: 'transparent' }}>
-      <ReactApexChart options={options} series={[{data:series}]} type="bar" height={460} />
+      <ReactApexChart options={options} series={buildSeries(categories, spent, budgeted, theme)} type="bar" height={80 + 50 * categories.length} />
     </Box>
   );
+}
+
+function buildSeries(categories, spent, budgeted, theme) {
+  // let d = [] 10=570, 2=180,
+  // for (let i = 0; i < spent.length; i++) {
+  //   d.push({
+  //     x: categories[i],
+  //     y: spent[i],
+  //     fillColor: spent[i] <= budgeted[i] ? theme.palette.primary.main : theme.palette.error.main,
+  //     // goals: [
+  //     //   {
+  //     //     name: "budgeted",
+  //     //     value: budgeted[i],
+  //     //     strokeWidth: 5,
+  //     //     strokeColor: theme.palette.primary.dark
+  //     //   }
+  //     // ]
+  //   })
+  // }
+  return [{
+    name: "spent",
+    data: spent,
+    color: theme.palette.primary.main
+  }, {
+    name: "budgeted",
+    data: budgeted,
+    color: theme.palette.secondary.main
+  }]
 }
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -5,68 +5,156 @@ import Typography from '@mui/material/Typography';
 
 // project import
 import MainCard from 'components/MainCard';
-import MonthlyBarChart from './MonthlyBarChart';
 import SpendingCard from './SpendingCard';
-import RecentTransactionsTable from './RecentTransactionsTable';
 
 // assets
 import FileUpload from 'components/FileUpload';
 import {Link} from '@mui/material';
-import Box from "@mui/material/Box";
+import {useMemo, useState} from "react";
+import {useBudgets, useExpenditureAggregate} from "../../api/graph";
+import {startOfLastMonth} from "../../utils/dates";
+import MonthlyStat from "./MonthlyStat";
 
 // ==============================|| DASHBOARD - DEFAULT ||============================== //
 
 export default function DashboardDefault() {
-  return (
-      <Grid container rowSpacing={4.5} columnSpacing={2.75}>
-        {/* row 1 */}
-        <Grid item xs={12} md={7} lg={8}>
-          <SpendingCard />
-        </Grid>
-        <Grid item xs={12} md={5} lg={4}>
-          <Typography variant="h5">Top 10 categories</Typography>
-          <MainCard sx={{ mt: 2 }} content={false}>
-            <Box sx={{ pt: 1, pr: 2 }}>
-              <MonthlyBarChart />
-            </Box>
-          </MainCard>
-        </Grid>
+    const [totalSpent, setTotalSpent] = useState([0,0])
+    const [adherance, setAdherance] = useState(1)
+    const [cashflow, setCashflow] = useState([0, 0])
+    const [rewards, setRewards] = useState(0)
 
-        {/* row 2 */}
-        <Grid item xs={12} md={10} lg={8}>
-          <Grid container alignItems="center" justifyContent="space-between">
-            <Grid item>
-              <Typography variant="h5">Recent Transactions</Typography>
+    const monthlySpending = useExpenditureAggregate({
+        since: startOfLastMonth(),
+        until: new Date(),
+        span: "MONTH",
+        groupBy: "NONE",
+    })
+
+    const groupedMonthlySpending= useExpenditureAggregate({
+        since: startOfLastMonth(),
+        until: new Date(),
+        span: "MONTH",
+        groupBy: "BUDGET_CATEGORY",
+    })
+
+    const budgets = useBudgets(1)
+
+    // calculate the total spent this and last month
+    useMemo(() => {
+        if (monthlySpending.data) {
+            const now = new Date()
+            let [lastMonth, thisMonth] = totalSpent
+            monthlySpending.data.aggregatedExpenditures.forEach(e => {
+                const start = new Date(e.spanStart)
+                if (start.getMonth() === now.getMonth()) {
+                    thisMonth = e.amount
+                } else {
+                    lastMonth = e.amount
+                }
+            })
+            setTotalSpent([lastMonth, thisMonth])
+        }
+    }, [monthlySpending.data])
+
+    // calculate the cash flow as a function of the total budgeted income and previously calculated spending
+    useMemo(() => {
+        if (budgets.data && budgets.data.budgets.length > 0) {
+            const budget = budgets.data.budgets[0]
+            const totalIncome = budget.incomes.reduce((sum, e) => sum + e.amount, 0)
+
+            setCashflow(totalSpent.map(spent => totalIncome - spent))
+        }
+    }, [budgets.data, totalSpent])
+
+    // find the budget adherance by counting the number of categories in budget
+    useMemo(() => {
+        if (budgets.data && budgets.data.budgets.length > 0) {
+            const budget = budgets.data.budgets[0]
+            const categories = budget.expenses.length
+            let overbudget = 0
+
+            // build a map
+            let b = {}
+            budget.expenses.forEach(e => {
+                b[e.category] = e.amount
+            })
+
+            groupedMonthlySpending.data.aggregatedExpenditures.forEach(e => {
+                if (b[e.groupByCategory] < e.amount) {
+                    overbudget += 1
+                }
+            })
+
+            setAdherance((categories - overbudget) / categories)
+        }
+    }, [groupedMonthlySpending.data, budgets.data])
+
+
+    return (
+        <Grid container rowSpacing={4.5} columnSpacing={2.75}>
+            {/* row 1 */}
+            <Grid item xs={12} sx={{ mb: -2.25 }}>
+                <Typography variant="h5">Overview</Typography>
             </Grid>
-            <Grid item />
-          </Grid>
-          <MainCard sx={{ mt: 2 }} content={false}>
-            <RecentTransactionsTable />
-          </MainCard>
-        </Grid>
-        <Grid item xs={12} md={5} lg={4}>
-          <Grid container alignItems="center" justifyContent="space-between">
-            <Grid item>
-              <Typography variant="h5">Import Transactions</Typography>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+                <MonthlyStat title="Total Monthly Expenditure"
+                             prev={totalSpent[0]}
+                             current={totalSpent[1]}
+                             format={(v) => '$' + Math.abs(v).toFixed(2)}
+                             positiveMessage="Your spending increased by"
+                             negativeMessage="Your spendig decreased by" />
             </Grid>
-            <Grid item />
-          </Grid>
-          <MainCard sx={{ mt: 2 }}
-            title="Upload CSV"
-            secondary={
-              <Link color="primary" href="/">Download CSV Template</Link>
-            }
-          >
-            <Stack spacing={3}>
-              <Typography variant="caption" color="text.secondary" gutterBottom>
-                Import transactions from a CSV file.
-              </Typography>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+                <MonthlyStat title="Cashflow"
+                             prev={cashflow[0]}
+                             current={cashflow[1]}
+                             format={(v) => '$' + Math.abs(v).toFixed(2)}
+                             positiveMessage="Your cashflow increased by"
+                             negativeMessage="Your casfhlow decreased by" />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+                <MonthlyStat title="Budget Adherence"
+                             current={adherance}
+                             showDiff={false}
+                             format={(v) => v.toFixed(0)*100 + '%'}
+                             positiveMessage="You stuck to your budget"/>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+                <MonthlyStat title="Rewards"
+                             current={0}
+                             showDiff={false}
+                             format={(v) => '$' + Math.abs(v).toFixed(2)}
+                             positiveMessage="This is not yet implemnted."/>
+            </Grid>
 
-              <FileUpload />
-            </Stack>
-          </MainCard>
+            {/* row 2 */}
+            <Grid item xs={12}>
+                <SpendingCard/>
+            </Grid>
+
+            {/* row 3 */}
+            <Grid item xs={12}>
+                <Grid container alignItems="center" justifyContent="space-between">
+                    <Grid item>
+                        <Typography variant="h5">Import Transactions</Typography>
+                    </Grid>
+                    <Grid item/>
+                </Grid>
+                <MainCard sx={{mt: 2}}
+                          title="Upload CSV"
+                          secondary={
+                              <Link color="primary" href="/">Download CSV Template</Link>
+                          }
+                >
+                    <Stack spacing={3}>
+                        <Typography variant="caption" color="text.secondary" gutterBottom>
+                            Import transactions from a CSV file.
+                        </Typography>
+
+                        <FileUpload/>
+                    </Stack>
+                </MainCard>
+            </Grid>
         </Grid>
-
-      </Grid>
-  );
+    );
 }
