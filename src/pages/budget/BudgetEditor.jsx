5ba68108016f6ae8c0058746aecb651fc307d121
Ben <ben.zhao@benzhao.dev>
2024-09-27 19:26:38 -0400

    budget page functional layout no backend

---
+++ src/pages/budget/BudgetEditor.jsx
@@ -0,0 +1,189 @@
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import {Fab, Switch} from "@mui/material";
+import {DeleteOutlined, PlusOutlined} from "@ant-design/icons";
+import Button from "@mui/material/Button";
+import Grid2 from "@mui/material/Unstable_Grid2";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import {useMemo} from "react";
+
+function Income({budget, setBudget, index}) {
+    const income = budget.incomes[index];
+
+    let changeSource = e => {
+        income.source = e.target.value
+        setBudget(budget)
+    }
+
+    let changeAmount = e => {
+        income.amount = parseFloat(e.target.value)
+        setBudget(budget)
+    }
+
+    return (
+        <Grid2 container spacing={2} sx={{ justifyContent: 'space-between' }}>
+            <Grid2 xs={5}>
+                <TextField id={"income-source-" + index}
+                           label="Source"
+                           variant="standard"
+                           defaultValue={income.source}
+                           fullWidth={true}
+                           onChange={changeSource}/>
+            </Grid2>
+
+            <Grid2 xs={6}>
+                <TextField id={"income-amount-" + index}
+                           label="Amount"
+                           variant="standard"
+                           defaultValue={income.amount}
+                           fullWidth={true}
+                           onChange={changeAmount}/>
+            </Grid2>
+
+            <Grid2 sx={{ justifyContent: 'flex-end' }} xs={1}>
+                <Fab color="secondary" aria-label="remove" size="small" onClick={() => {
+                    budget.incomes.splice(index, 1)
+                    setBudget(budget)
+                }}>
+                    <DeleteOutlined style={{ fontSize: '1.3rem' }} />
+                </Fab>
+            </Grid2>
+        </Grid2>
+    )
+}
+
+function Expense({budget, setBudget, index}) {
+    const expense = budget.expenses[index];
+
+    let changeCategory = e => {
+        expense.category = e.target.value
+        setBudget(budget)
+    }
+
+    let changeAmount = e => {
+        expense.amount = parseFloat(e.target.value)
+        setBudget(budget)
+    }
+
+    let toggleFixed = e => {
+        expense.isFixed = e.target.checked
+        setBudget(budget)
+        console.log(budget, e)
+    }
+
+    return (
+        <Grid2 container spacing={2} sx={{ justifyContent: 'space-between' }}>
+            <Grid2 xs={4}>
+                <TextField id={"expense-category-" + index}
+                           label="Category"
+                           variant="standard"
+                           defaultValue={expense.category}
+                           fullWidth={true}
+                           onChange={changeCategory}/>
+            </Grid2>
+
+            <Grid2 xs={5}>
+                <TextField id={"expense-amount-" + index}
+                           label="Amount"
+                           variant="standard"
+                           defaultValue={expense.amount}
+                           fullWidth={true}
+                           onChange={changeAmount}/>
+            </Grid2>
+
+            <Grid2 xs={2}>
+                <FormControlLabel id={"expense-fixed-" + index}
+                                  control={<Switch checked={expense.isFixed} />}
+                                  label="Fixed"
+                                  onChange={toggleFixed}/>
+            </Grid2>
+
+            <Grid2 sx={{ justifyContent: 'flex-end' }} xs={1}>
+                <Fab color="secondary" aria-label="remove" size="small" onClick={() => {
+                    budget.expenses.splice(index, 1)
+                    setBudget(budget)
+                }}>
+                    <DeleteOutlined style={{ fontSize: '1.3rem' }} />
+                </Fab>
+            </Grid2>
+        </Grid2>
+    )
+}
+
+export default function BudgetEditor({budget, setBudget, saveBudget}) {
+    let totalIncome = budget.incomes.reduce((a, b) => a + b.amount, 0)
+    let totalExpenses = budget.expenses.reduce((a, b) => a + b.amount, 0)
+
+    useMemo(() => {
+        totalIncome = budget.incomes.reduce((a, b) => a + b.amount, 0)
+        totalExpenses = budget.expenses.reduce((a, b) => a + b.amount, 0)
+    }, [budget])
+
+    return (
+        <Stack spacing={5}>
+            {/*Name*/}
+            <TextField id="budgetName" label="Budget Name"  variant="standard" fullWidth={true} defaultValue={budget.name} />
+
+            {/*Incomes*/}
+            <Grid2>
+                <Typography variant="subtitle1">Incomes</Typography>
+                <Stack spacing={2}>
+                    {
+                        budget.incomes.map((_, index) =>
+                            <Income key={index} budget={budget} setBudget={setBudget} index={index} />)
+                    }
+
+                    <Grid2>
+                        <Fab color="primary" aria-label="add" size="small" onClick={() => {
+                            budget.incomes.push({source: '', amount: 0})
+                            setBudget(budget)
+                        }}>
+                            <PlusOutlined style={{ fontSize: '1.3rem' }} />
+                        </Fab>
+                    </Grid2>
+                </Stack>
+            </Grid2>
+
+            {/*Income total*/}
+            <Stack spacing={4} direction="row">
+                <Typography variant="subtitle2">Total Income: </Typography>
+                <Typography variant="subtitle1" color="primary">${totalIncome}</Typography>
+            </Stack>
+
+            {/*Expenses*/}
+            <Grid2>
+                <Typography variant="subtitle1">Expenses</Typography>
+                <Stack spacing={2}>
+                    {
+                        budget.expenses.map((_, index) =>
+                            <Expense key={index} budget={budget} setBudget={setBudget} index={index} />)
+                    }
+
+                    <Grid2>
+                        <Fab color="primary" aria-label="add" size="small" onClick={() => {
+                            budget.expenses.push({category: '', amount: 0, isFixed: false})
+                            setBudget(budget)
+                        }}>
+                            <PlusOutlined style={{ fontSize: '1.3rem' }} />
+                        </Fab>
+                    </Grid2>
+                </Stack>
+            </Grid2>
+
+            {/*Total Expenses*/}
+            <Stack spacing={4} direction="row">
+                <Typography variant="subtitle2">Total Expenses: </Typography>
+                <Typography variant="subtitle1" color="primary">${totalExpenses}</Typography>
+            </Stack>
+
+            {/*Total Expenses*/}
+            <Stack spacing={4} direction="row">
+                <Typography variant="subtitle2">Extra: </Typography>
+                <Typography variant="subtitle1" color={totalIncome >= totalExpenses ? "primary" : "error"}>${totalIncome - totalExpenses}</Typography>
+            </Stack>
+
+            <Button variant="contained" onClick={saveBudget}>Save</Button>
+        </Stack>
+    )
+}
--- src/pages/budget/index.jsx
+++ src/pages/budget/index.jsx
@@ -2,32 +2,69 @@
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import MainCard from "../../components/MainCard";
-import MonthlyBarChart from "../dashboard/MonthlyBarChart";
 import Box from "@mui/material/Box";
 import BudgetPieChart from "./BudgetPieChart";
+import {useBudgets} from "../../api/graph";
+import {useState} from "react";
+import BudgetEditor from "./BudgetEditor";
+import {clone} from "lodash";
 
 // project import
-
-// assets
+const templateBudget = {
+    name: "My Budget",
+    incomes: [{
+        source: "Work",
+        amount: 2400.00
+    }],
+    expenses: [
+        {
+            category: "Rent",
+            amount: 1000.00,
+            isFixed: true,
+            isSlack: false
+        },
+        {
+            category: "Groceries",
+            amount: 600.00,
+            isFixed: false,
+            isSlack: false
+        },
+        {
+            category: "Savings",
+            amount: 800.00,
+            isFixed: false,
+            isSlack: true
+        },
+    ]
+}
 
 // ==============================|| DASHBOARD - DEFAULT ||============================== //
 
 export default function BudgetDashboard() {
+    const {data} = useBudgets(1)
+    const [budget, setBudget] = useState(templateBudget)
+    const hasBudget = data && data.budgets && data.budgets.length === 0
+
     return (
         <>
         <Typography variant="h5">Budget</Typography>
+            {hasBudget &&
+            <Typography variant="subtitle2" color='secondary'>
+                Looks like you don't have a budget saved. Here's a basic template to get you started.
+            </Typography>
+            }
         <Grid container rowSpacing={4.5} columnSpacing={2.75}>
-            <Grid item xs={12} md={7} lg={8}>
+            <Grid item xs={12} md={6}>
                 <MainCard content={false} sx={{ mt: 2 }}>
                     <Box sx={{ pt: 1, pr: 2 }}>
-                        <BudgetPieChart />
+                        <BudgetPieChart budget={budget} />
                     </Box>
                 </MainCard>
             </Grid>
-            <Grid item xs={12} md={5} lg={4}>
-                <MainCard sx={{ mt: 2 }} content={false}>
+            <Grid item xs={12} md={6}>
+                <MainCard title="Budget Editor" sx={{ mt: 2 }} content={true}>
                     <Box sx={{ pt: 1, pr: 2 }}>
-                        <MonthlyBarChart />
+                        <BudgetEditor budget={budget} setBudget={b => setBudget(clone(b))} saveBudget={e => console.log("SAVE")} />
                     </Box>
                 </MainCard>
             </Grid>
