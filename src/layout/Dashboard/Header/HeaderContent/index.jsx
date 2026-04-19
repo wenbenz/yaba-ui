eed32c38aceba5f7e1fa065c02b5a00e345695c1
Ben <ben.zhao@benzhao.dev>
2026-04-19 01:23:41 -0400

    delete dead header components (Notification, Search, MobileSection) (#45)
    Generated by dev pipeline agents.
    Requirements: REQ-001, REQ-002
    Tests: no test suite configured; lint and build pass locally.

--- src/layout/Dashboard/Header/HeaderContent/index.jsx
+++ src/layout/Dashboard/Header/HeaderContent/index.jsx
@@ -1,30 +1,5 @@
-// ==============================|| HEADER - CONTENT ||============================== //
-
 import Profile from "./Profile";
 
 export default function HeaderContent() {
-  // const downLG = useMediaQuery((theme) => theme.breakpoints.down("lg"));
-
-  return (
-    <>
-      {/*{!downLG && <Search />}*/}
-      {/*{downLG && <Box sx={{ width: '100%', ml: 1 }} />}*/}
-      {/*<IconButton*/}
-      {/*  component={Link}*/}
-      {/*  href="https://github.com/codedthemes/mantis-free-react-admin-template"*/}
-      {/*  target="_blank"*/}
-      {/*  disableRipple*/}
-      {/*  color="secondary"*/}
-      {/*  title="Download Free Version"*/}
-      {/*  sx={{ color: 'text.primary', bgcolor: 'grey.100' }}*/}
-      {/*>*/}
-      {/*  <GithubOutlined />*/}
-      {/*</IconButton>*/}
-
-      {/*<Notification />*/}
-      <Profile />
-      {/*{!downLG && <Profile />}*/}
-      {/*{downLG && <MobileSection />}*/}
-    </>
-  );
+  return <Profile />;
 }
